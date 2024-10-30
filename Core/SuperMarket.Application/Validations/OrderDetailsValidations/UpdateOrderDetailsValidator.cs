d3d7deadc1f21c8d46f7b554093ed0ce737f73e6
Mibrahim2004 <muradliibrahim161616@gmail.com>
2024-10-30 14:14:59 +0400

    Some Validations added

--- Core/SuperMarket.Application/Validations/OrderDetailsValidations/UpdateOrderDetailsValidator.cs
+++ Core/SuperMarket.Application/Validations/OrderDetailsValidations/UpdateOrderDetailsValidator.cs
@@ -12,7 +12,14 @@ namespace SuperMarket.Application.Validations.OrderDetailsValidations
     {
         public UpdateOrderDetailsValidator()
         {
-            
+            RuleFor(x => x.OrderId)
+          .GreaterThan(0).WithMessage("Order ID must be greater than 0.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage("The quantity must be greater than 0.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("The Price must be greater than 0.");
         }
     }
 }
--- Core/SuperMarket.Application/Validations/PaymentValidations/CreatePaymentValidator.cs
+++ Core/SuperMarket.Application/Validations/PaymentValidations/CreatePaymentValidator.cs
@@ -12,7 +12,19 @@ namespace SuperMarket.Application.Validations.PaymentValidators
     {
         public CreatePaymentValidator()
         {
-            
+            RuleFor(x => x.OrderId)
+           .GreaterThan(0).WithMessage("Order ID must be greater than 0.");
+
+            RuleFor(x => x.PaymentDate)
+                .LessThanOrEqualTo(DateTime.Now).WithMessage("Payment date cannot be in the future.");
+
+            RuleFor(x => x.PaymentMethod)
+                .NotEmpty().WithMessage("Payment method is required.")
+                .Length(2, 40).WithMessage("Payment method must be between 2 and 40 characters.");
+
+            RuleFor(x => x.Amount)
+                .GreaterThan(0).WithMessage("Payment amount must be greater than 0.");
+
         }
     }
 }
